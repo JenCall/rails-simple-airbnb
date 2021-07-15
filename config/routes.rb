4a34ff51e13070a727c4fc012ee1c566eac4f2a3
Jeniffer Callou <jenifferprc@gmail.com>
2021-07-15 20:32:57 +0200

    Rails Simple Airbnb - solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :flats
+
+  get '/flats', to: 'flats#index'
+
+  post '/flats', to: 'flats#create'
+
+  get 'flats/new', to: 'flats#new', as: :new_flat
+
+  get 'flats/:id/edit', to: 'flats#edit', as: :edit_flat
+
+  get 'flats/:id', to: 'flats#show', as: :flat
+
+  patch 'flats/:id', to: 'flats#update'
+
+  put 'flats/:id', to: 'flats#update'
+
+  delete 'flats/:id', to: 'flats#destroy', as: :delete
 end
